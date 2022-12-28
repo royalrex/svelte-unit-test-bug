5619446f6d0967f9380dd0ae2cc3335f6652b4cc
Sebbe Selvig <sebbe.selvig@storytel.com>
2022-12-28 12:27:59 +0100

    Format

--- src/ReactiveComponent.spec.js
+++ src/ReactiveComponent.spec.js
@@ -17,38 +17,40 @@ const html = `
 `;
 //language=CSS
 const style = `
-#container {
-  background: white;
-  padding: 1em;
-  border: 3px solid black;
-}
+    #container {
+        background: white;
+        padding: 1em;
+        border: 3px solid black;
+    }
 `;
 
 describe('Reactive component using data from a store', () => {
-  // first test check that we can mount with default value and print "nothing"
-  it('should work on first mount', () => {
-    mount(ReactiveComponent, {});
-    cy.contains('Case default').should('not.exist');
-  });
+    // first test check that we can mount with default value and print "nothing"
+    it('should work on first mount', () => {
+        mount(ReactiveComponent, {});
+        cy.contains('Case default').should('not.exist');
+    });
 
-  // on second mount we expect the component to write "Default case"
-  it('should work on second mount',  () => {
-    mount(ReactiveComponent, {},{html
+    // on second mount we expect the component to write "Default case"
+    it('should work on second mount', () => {
+        mount(ReactiveComponent, {}, {
+            html
+        });
+        testStore.set({
+            numberValue: 1,
+        });
+        cy.contains('Case default').should('be.visible');
     });
-    testStore.set({
-      numberValue: 1,
-    });
-    cy.contains('Case default').should('be.visible');
-  });
 
-  // on third mount, we tweak the store value so that 'Case default' is no longer visible and 'Case 2' should be visible
-  // But it seems that the reactive nature listening
-  it('should work on third mount', () => {
-    mount(ReactiveComponent, {}, {html: '<div id="container"><div id="here"></div></div>', style
+    // on third mount, we tweak the store value so that 'Case default' is no longer visible and 'Case 2' should be visible
+    // But it seems that the reactive nature listening
+    it('should work on third mount', () => {
+        mount(ReactiveComponent, {}, {
+            html: '<div id="container"><div id="here"></div></div>', style
+        });
+        testStore.set({
+            numberValue: 2,
+        });
+        cy.contains('Case 2').should('be.visible');
     });
-    testStore.set({
-      numberValue: 2,
-    });
-    cy.contains('Case 2').should('be.visible');
-  });
 });
