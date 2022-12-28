69818dbe7bf19eec78ab0d2856c04d9be98e0cb0
Sebbe Selvig <sebbe.selvig@storytel.com>
2022-12-28 12:27:49 +0100

    Extended test case

--- src/ReactiveComponent.spec.js
+++ src/ReactiveComponent.spec.js
@@ -11,30 +11,44 @@ import { testStore } from './stores';
  *
  * It does not happen when there is only two cases.
  */
+//<div id="here"></div>
+//language=HTML prefix=<body> suffix=</body>
+const html = `
+`;
+//language=CSS
+const style = `
+#container {
+  background: white;
+  padding: 1em;
+  border: 3px solid black;
+}
+`;
 
 describe('Reactive component using data from a store', () => {
   // first test check that we can mount with default value and print "nothing"
   it('should work on first mount', () => {
-    mount(ReactiveComponent);
+    mount(ReactiveComponent, {});
     cy.contains('Case default').should('not.exist');
   });
 
   // on second mount we expect the component to write "Default case"
   it('should work on second mount',  () => {
-    mount(ReactiveComponent);
+    mount(ReactiveComponent, {},{html
+    });
     testStore.set({
       numberValue: 1,
     });
-    cy.contains('Case default');
+    cy.contains('Case default').should('be.visible');
   });
 
   // on third mount, we tweak the store value so that 'Case default' is no longer visible and 'Case 2' should be visible
   // But it seems that the reactive nature listening
   it('should work on third mount', () => {
-    mount(ReactiveComponent);
+    mount(ReactiveComponent, {}, {html: '<div id="container"><div id="here"></div></div>', style
+    });
     testStore.set({
       numberValue: 2,
     });
-    cy.contains('Case 2');
+    cy.contains('Case 2').should('be.visible');
   });
 });
